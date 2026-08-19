d32ce3175aca87eaeabf03a976fbbdc7f32b4908
hyeonji817 <guswl930817@gmail.com>
2026-08-19 17:03:50 +0900

    겹치는 현상 원인 제거

--- client/src/main/Products.tsx
+++ client/src/main/Products.tsx
@@ -1,20 +1,12 @@
-import "./Products.css"; 
-import Header from "./Header";
+import "./Products.css";
 import Product_List from "./Product_List";
-import Footer from "./Footer";
 
 const Products = () => {
   return (
     <div className="Products_wrap">
-      <div className="Products_Header">
-        <Header />  
-      </div>     {/** Products_Header end */}
       <div className="Product_List">
         <Product_List />  
       </div>        {/** Product_List end */}
-      <div className="Products_Footer">
-        <Footer />  
-      </div>     {/** Products_Footer end */}
     </div>     /** Products_wrap end */
   );
 };
