bf6ddc1bc3b1c071d4d19a732f70cc9a2c9b9256
hyeonji817 <guswl930817@gmail.com>
2026-08-30 00:54:08 +0900

    상품 상세 페이지(리빙용품) API/백엔드(수파베이스) 연동

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,21 +6,25 @@ import Products from "./main/Products";
 import Product_List from "./main/Product_List";
 
 import Products1 from "./main/Products1";
+import Product1 from "./shop/Product1";
 /** import Product1_1 from "./shop/Product1_1";
 import Product1_2 from "./shop/Product1_2";
 import Product1_3 from "./shop/Product1_3"; */
 
 import Products2 from "./main/Products2";
+//import Product2 from "./shop/Product2";
 import Product2_1 from "./shop/Product2_1";
 import Product2_2 from "./shop/Product2_2";
 import Product2_3 from "./shop/Product2_3";
 
 import Products3 from "./main/Products3";
+//import Product3 from "./shop/Product3";
 import Product3_1 from "./shop/Product3_1";
 import Product3_2 from "./shop/Product3_2";
-import Product3_3 from "./shop/Product3_3";
+import Product3_3 from "./shop/Product3_3"; 
 
 import Products4 from "./main/Products4";
+//import Product4 from "./shop/Product4";
 import Product4_1 from "./shop/Product4_1";
 import Product4_2 from "./shop/Product4_2";
 
@@ -33,19 +37,22 @@ function App() {
         <Route path="/product_list" element={<Product_List />} />
 
         <Route path="/products_living" element={<Products1 />} />
-        <Route path="/airmesh" element={<Product1_1 />} />
+        <Route path="/airmesh" element={<Product1 productSlug="air-mesh-cool-pad" />} />
+        <Route path="/ironCat_Pole" element={<Product1 productSlug="iron-cat-pole" />} />
+        <Route path="/puppy_Stairs" element={<Product1 productSlug="puppy-stairs" />} />
+        {/** <Route path="/airmesh" element={<Product1_1 />} />
         <Route path="/ironCat_Pole" element={<Product1_2 />} />
-        <Route path="/puppy_Stairs" element={<Product1_3 />} />
+        <Route path="/puppy_Stairs" element={<Product1_3 />} /> */}
 
         <Route path="/products_food" element={<Products2 />} />
         <Route path="/dograng_classic" element={<Product2_1 />} />
         <Route path="/mojjine_food" element={<Product2_2 />} />
-        <Route path="/rorench_petChurr" element={<Product2_3 />} />
+        <Route path="/rorench_petChurr" element={<Product2_3 />} /> 
 
         <Route path="/products_dress" element={<Products3 />} />
         <Route path="/geumhwadan_hanbok" element={<Product3_1 />} />
         <Route path="/aricat_Tshirt" element={<Product3_2 />} />
-        <Route path="/cherryPola_Tshirt" element={<Product3_3 />} />
+        <Route path="/cherryPola_Tshirt" element={<Product3_3 />} /> 
 
         <Route path="/products_medicine" element={<Products4 />} />
         <Route path="/doctorOne_8" element={<Product4_1 />} />
